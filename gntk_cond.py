9fb87edde47a903d03d2d50487b812eee292bdd8
Unknown-191021 <141397835+Unknown-191021@users.noreply.github.com>
2023-08-10 11:49:59 +1000

    Update gntk_cond.py

--- gntk_cond.py
+++ gntk_cond.py
@@ -65,11 +65,6 @@ class GNTK(object):
         return adj_block.dot(S.reshape(-1)).reshape(N1, N2) * scale_mat
 
     def show_memory(self, unit='KB', threshold=1):
-        '''查看变量占用内存情况
-
-        :param unit: 显示的单位，可为`B`,`KB`,`MB`,`GB`
-        :param threshold: 仅显示内存数值大于等于threshold的变量
-        '''
         from sys import getsizeof
         scale = {
             'B': 1, 'KB': 1024, 'MB': 1048576, 'GB': 1073741824}[unit]
